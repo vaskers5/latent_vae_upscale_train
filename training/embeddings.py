216db996c38a94a690793a96be7fc6b615104bd3
vaskers5 <danil.krokodil@mail.ru>
2025-10-14 21:22:33 +0000

    delete legacy

--- training/embeddings.py
+++
@@ -1,7 +0,0 @@
-"""Backward compatible shim after merging embedding utilities into a single module."""
-
-from __future__ import annotations
-
-from .embedding_io import CACHE_VERSION, TransformParams, save_record
-
-__all__ = ["TransformParams", "save_record", "CACHE_VERSION"]
